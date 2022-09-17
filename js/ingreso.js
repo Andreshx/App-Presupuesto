0ec5a9b368714c2a19cd7f0fd927d7ac88a1f3fd
AndresHx <andreshx1996-e@hotmail.com>
2022-09-17 00:13:51 -0500

    Second Proyect

---
+++ js/ingreso.js
@@ -0,0 +1,16 @@
+ class Ingreso extends Datos { 
+    static contadorIngresos  = 0 ; 
+
+    constructor(descripcion, valor){ 
+        super(descripcion,valor); 
+        this._id = ++Ingreso.contadorIngresos;
+
+    }
+
+    get id(){ 
+        return this._id ; 
+    }
+
+
+
+ }
